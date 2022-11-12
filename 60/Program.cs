20871c497db9b491a928896aa4659a15ef64ebef
Korvyn <korvyn555@google.com>
2022-11-12 15:35:40 +0300

    60

--- 60/Program.cs
+++ 60/Program.cs
@@ -9,3 +9,39 @@
 26(1,0,1) 55(1,1,1) */
 
 
+void InputMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(1); k++)
+                matrix[i, j, k] = new Random().Next(10, 99);
+
+        }
+    }
+}
+
+void PrintMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(1); k++)
+                Console.WriteLine(matrix[i, j, k] + " (" + i + ", " + j + ", " + k + ")");
+
+        }
+    }
+}
+
+Console.Clear();
+Console.Write("Введите координату X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату Z: ");
+int z = Convert.ToInt32(Console.ReadLine());
+int[,,] matrix = new int[x, y, z];
+InputMatrix(matrix);
+PrintMatrix(matrix);
--- 62/Program.cs
+++ 62/Program.cs
@@ -5,3 +5,15 @@
 11 16 15 06
 10 09 08 07   */
 
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(0, 11);
+            Console.Write(matrix[i, j] + " \t");
+        }
+        Console.WriteLine();
+    }
+}
