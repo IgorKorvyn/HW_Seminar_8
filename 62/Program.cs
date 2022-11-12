8802d471363f9d9940389de033078550a8cff5ad
Korvyn <korvyn555@google.com>
2022-11-12 16:05:03 +0300

    662

--- 62/Program.cs
+++ 62/Program.cs
@@ -7,13 +7,35 @@
 
 void InputMatrix(int[,] matrix)
 {
-    for (int i = 0; i < matrix.GetLength(0); i++)
+    int temp = 1, i = 0, j = 0;
+    while (temp <= matrix.GetLength(0) * matrix.GetLength(1))
     {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            matrix[i, j] = new Random().Next(0, 11);
+        matrix[i, j] = temp;
+        temp++;
+        if (i <= j + 1 && i + j < matrix.GetLength(1) - 1)
+            j++;
+        else if (i < j && i + j >= matrix.GetLength(0) - 1)
+            i++;
+        else if (i >= j && i + j > matrix.GetLength(1) - 1)
+            j--;
+        else
+            i--;
+
+    }
+    for (i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (j = 0; j < matrix.GetLength(1); j++)
             Console.Write(matrix[i, j] + " \t");
-        }
         Console.WriteLine();
     }
+
 }
+
+
+Console.Clear();
+Console.Write("Введите кол-во строк или столбцов квадратного массива : ");
+int n = Convert.ToInt32(Console.ReadLine());
+int m = n;
+int[,] matrix = new int[n, m];
+Console.WriteLine($"Спирально заполненный массив: ");
+InputMatrix(matrix);
